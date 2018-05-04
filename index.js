58172938c6f0238348d29e69f6a1a74d04bff80d
zvasdfg <alejandro@qurau.mx>
2018-05-04 09:57:23 -0500

    First Commit

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
+
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accesToken, refreshToken, profile, done) => {
+        console.log('accesToken:', accesToken)
+        console.log('refreshToken:', refreshToken)
+        console.log('profile:', profile);
+    })
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile','email']
+    })
+ );
+
+ app.get('/auth/google/callback',passport.authenticate('google'))
+
 
 app.get('/', (req,res) => {
 res.send({bye : 'Buddy'});
 });
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+console.log(PORT)
